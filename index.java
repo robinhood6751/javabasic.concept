eb960cb90ce3402d50df3de99075ad231d89bd75
MOHD SOHAIL <107904324+robinhood6751@users.noreply.github.com>
2024-03-16 15:13:32 +0530

    Add files via upload

---
+++ index.java
@@ -0,0 +1,71 @@
+// This is the nested for looop and that make the inverse pattern to understand that how the nested loops work together and here either we can take 
+// the input from the user or set a variable named as n;
+
+// import java.util.*;
+
+// public class index {
+
+//   public static void main(String[] args) {
+//     System.out.print("Enter the number of stars : ");
+//     Scanner scn = new Scanner(System.in);
+//     int n = scn.nextInt();
+//     System.out.print("Enter the number of lines : ");
+//     int lines = scn.nextInt();
+
+//     // int n = 5;
+//     for (int line = 1; line <= lines; line++) {
+//       for (int star = 1; star <= (n - line + 1); star++) {
+//         System.out.print("*");
+//       }
+//       System.out.println();
+//     }
+//   }
+// }
+// This is also a nested for loop and that make a increasing simple pattern to understand the loops
+// public class index {
+//   public static void main(String args[]) {
+//     System.out.println("Enter the numbers of stars : ");
+//     for(int line = 1; line <= 5; line++){
+//       for(int star = 1; star <= line; star++){
+//         System.out.print("*");
+//       }
+//       System.out.println();
+//     }
+//   }
+// }
+
+// Here this is the half pyramid of numbers to understand the concept of nested loop
+
+
+// public class index{
+//   public static void main(String[] args) {
+//     int n = 7;
+//     for(int line = 1; line <= n; line++){
+//       for(int num = 1; num<= line; num++){
+//         System.out.print(num);
+//       }
+//       System.out.println();
+//     }
+//   }
+// } 
+
+
+
+// here we are going to print the half pyramid of characters to understand the concept
+
+// public class index{
+//   public static void main (String[] args){
+//     // variables 
+//     int n = 4;
+//     char ch = 'A';
+//     // outer loop
+//     for(int line = 1; line <= n; line++){
+//       // inner loop
+//       for(int chars = 1; chars <= line; chars++){
+//         System.out.print(ch);
+//         ch++;
+//       }
+//       System.out.println();
+//     }
+//   }
+// }
